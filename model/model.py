f1719d3f1e2e18017c4a02db20cdc5d49f59487c
bendorevitch <bendorevitch@gmail.com>
2023-12-18 22:12:54 +1100

    minor changes

---
+++ model/model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+games_details = pd.read_csv('model/Data/games_details.csv')
+ranking = pd.read_csv('model/Data/ranking.csv')
+teams= pd.read_csv('model/Data/teams.csv')
+
+
+
+
+
+
+
