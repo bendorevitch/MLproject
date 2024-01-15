874d0ac0ed49b6d371eeb6577d1a778cd94a368b
bendorevitch <bendorevitch@gmail.com>
2024-01-15 23:32:01 +1100

    can get scoreboard and box score up now

--- app/app_control.py
+++ app/app_control.py
@@ -1,20 +1,48 @@
-import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
 from app_functions import app_functions
 
-app = tk.Tk()
-app.title("Python Desktop App")
-app.geometry("800x600")
 
-ap = app_functions()
+root = Tk()
+root.geometry("600x200")
+root.title('percent chance of winning')
 
-button = tk.Button(
-    text="Click me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-    command=ap.get_scoreboard
-)
-button.pack()
+def get_scoreboard_window():
+    scoreboard = Toplevel(root)
 
-app.mainloop()
+    scoreboard.title("New Window")
+ 
+    # sets the geometry of toplevel
+    scoreboard.geometry("600x200")
+
+    ap = app_functions
+    sb_date , games, ids = ap.get_scoreboard()
+
+    # A Label widget to show in toplevel
+    Label(scoreboard, 
+          text =sb_date).pack()
+    for (item, id) in zip(games, ids):
+        button = Button(scoreboard,text=item,command=lambda x=id :get_boxscore_window(x))
+        button.pack()
+
+def get_boxscore_window(id):
+    boxscore = Toplevel(root)
+
+    boxscore.title("New Window")
+ 
+    # sets the geometry of toplevel
+    boxscore.geometry("600x200")
+
+    ap = app_functions
+    probs = ap.get_boxscore(id)
+
+    # A Label widget to show in toplevel
+    Label(boxscore, 
+          text =probs).pack()
+
+
+
+get_today_scoreboard = Button(root, text="get todays games", command = get_scoreboard_window)
+get_today_scoreboard.pack()
+
+root.mainloop()
--- app/app_functions.py
+++ app/app_functions.py
@@ -1,75 +1,78 @@
 from nba_api.live.nba.endpoints import scoreboard, boxscore
 from datetime import timezone
 from dateutil import parser
+
 import pandas as pd
 import pickle
 import tkinter as tk
 
+
 class app_functions():
-    def get_scoreboard(self):
+    def get_scoreboard():
+        f = "{awayTeam} vs. {homeTeam} @ {gameTimeLTZ}" 
         board = scoreboard.ScoreBoard()
-        self.game_dates= ("ScoreBoardDate: " + board.score_board_date)
+        game_dates= ("ScoreBoardDate: " + board.score_board_date)
+
         games = board.games.get_dict()
-        self.today_games = []
+        today_games = []
+        today_games_id = []
         for game in games:
             gameTimeLTZ = parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone(tz=None)
-            gameId=game['gameId']
-            awayTeam=game['awayTeam']['teamName']
-            homeTeam=game['homeTeam']['teamName']
-            self.today_games.append([gameId, awayTeam, homeTeam, gameTimeLTZ])
-        print(self.game_dates)
-        print(self.today_games)
+            today_games.append(f.format(awayTeam=game['awayTeam']['teamName'], homeTeam=game['homeTeam']['teamName'], gameTimeLTZ=gameTimeLTZ))
+            today_games_id.append(game['gameId'])
+        return game_dates ,today_games, today_games_id
+        
     
-    box = boxscore.BoxScore('0022300550') 
+    
+    def get_boxscore(id):
+        box = boxscore.BoxScore(id) 
 
-    away_stats = box.away_team_stats.get_dict()
-    home_stats = box.home_team_stats.get_dict()
+        away_stats = box.away_team_stats.get_dict()
+        home_stats = box.home_team_stats.get_dict()
 
-    df = pd.DataFrame(columns=['FG_PCT_home','FT_PCT_home','FG3_PCT_home','AST_home','REB_home','FG_PCT_away','FT_PCT_away','FG3_PCT_away','AST_away','REB_away'])
-    df_per = pd.DataFrame(columns=['away','home'])
-
-
-    stats_list = []
-    teams_list = []
-
-    #get home stats
-    stats_list.append(home_stats['statistics']['fieldGoalsPercentage'])
-    stats_list.append(home_stats['statistics']['freeThrowsPercentage'])
-    stats_list.append(home_stats['statistics']['threePointersPercentage'])
-    stats_list.append(home_stats['statistics']['assists'])
-    stats_list.append(home_stats['statistics']['reboundsTotal'])
-
-    #get away stats
-    stats_list.append(away_stats['statistics']['fieldGoalsPercentage'])
-    stats_list.append(away_stats['statistics']['freeThrowsPercentage'])
-    stats_list.append(away_stats['statistics']['threePointersPercentage'])
-    stats_list.append(away_stats['statistics']['assists'])
-    stats_list.append(away_stats['statistics']['reboundsTotal'])
-
-    #get team id
-    teams_list.append(away_stats['teamId'])
-    teams_list.append(home_stats['teamId'])
+        df = pd.DataFrame(columns=['FG_PCT_home','FT_PCT_home','FG3_PCT_home','AST_home','REB_home','FG_PCT_away','FT_PCT_away','FG3_PCT_away','AST_away','REB_away'])
+        df_per = pd.DataFrame(columns=['away','home'])
 
 
-    #getting game time
-    s = home_stats['statistics']['minutesCalculated']
-    for r in (("PT", ""), ("M", "")):
-        s = s.replace(*r)
-    s = 48/(int(s)/5)
+        stats_list = []
+        teams_list = []
 
-    df.loc[len(df)] = stats_list
-    df_per.loc[len(df_per)] = teams_list
+        #get home stats
+        stats_list.append(home_stats['statistics']['fieldGoalsPercentage'])
+        stats_list.append(home_stats['statistics']['freeThrowsPercentage'])
+        stats_list.append(home_stats['statistics']['threePointersPercentage'])
+        stats_list.append(home_stats['statistics']['assists'])
+        stats_list.append(home_stats['statistics']['reboundsTotal'])
 
-    to_multiply = ['AST_home','REB_home','AST_away','REB_away']
-    for x in to_multiply:
-        df[x] *= s
-        df[x] = df[x].round(0)
+        #get away stats
+        stats_list.append(away_stats['statistics']['fieldGoalsPercentage'])
+        stats_list.append(away_stats['statistics']['freeThrowsPercentage'])
+        stats_list.append(away_stats['statistics']['threePointersPercentage'])
+        stats_list.append(away_stats['statistics']['assists'])
+        stats_list.append(away_stats['statistics']['reboundsTotal'])
 
-    loaded_model = pickle.load(open('model/LogisticRegression.sav', 'rb'))
+        #get team id
+        teams_list.append(away_stats['teamId'])
+        teams_list.append(home_stats['teamId'])
 
-    score = loaded_model.predict_proba(df)
-    score_list = score.tolist()
-    df_per.loc[len(df_per)] = score_list[0]
 
-t = app_functions()
-t.get_scoreboard()
+        #getting game time
+        s = home_stats['statistics']['minutesCalculated']
+        for r in (("PT", ""), ("M", "")):
+            s = s.replace(*r)
+        s = 48/(int(s)/5)
+
+        df.loc[len(df)] = stats_list
+        df_per.loc[len(df_per)] = teams_list
+
+        to_multiply = ['AST_home','REB_home','AST_away','REB_away']
+        for x in to_multiply:
+            df[x] *= s
+            df[x] = df[x].round(0)
+
+        loaded_model = pickle.load(open('model/LogisticRegression.sav', 'rb'))
+
+        score = loaded_model.predict_proba(df)
+        score_list = score.tolist()
+        df_per.loc[len(df_per)] = score_list[0]
+        return df_per
